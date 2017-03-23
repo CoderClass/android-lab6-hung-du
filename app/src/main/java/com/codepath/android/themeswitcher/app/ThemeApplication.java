1d1d98ebf6b174cff73110cc1f63a8e2175ee0ce
udcun <duphungcong@gmail.com>
2017-03-23 19:54:46 +0700

    finish step 2

---
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeApplication.java
@@ -0,0 +1,13 @@
+package com.codepath.android.themeswitcher.app;
+
+import android.app.Application;
+
+/**
+ * Created by udcun on 3/23/2017.
+ */
+
+public class ThemeApplication extends Application {
+    // App level variable to retain selected spinner value
+    public static int currentPosition;
+
+}
